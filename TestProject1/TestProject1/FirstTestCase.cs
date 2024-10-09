573b53c4307efefd5b29bf1389c3d2859580aeb3
Opilane TTHK <opilane@tthk.ee>
2024-10-09 10:24:14 +0300

    a

---
+++ TestProject1/TestProject1/FirstTestCase.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TestProject1
+{
+    public  class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://store.steampowerd.com/";
+        }
+    }
+}
